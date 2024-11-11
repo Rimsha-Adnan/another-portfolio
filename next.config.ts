8b2b8960319b96ba6a57320ade6f9f71bc7bd600
rimsha-adnan <adnanhuzaifa@gmail.com>
2024-11-11 14:11:16 +0500

    changes

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
-import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-images :{
-  domains : ["www.shutterstock.com"]
-}
+import type { NextConfig } from "next";
+const nextConfig : NextConfig = {
+  images: {
+    domains: ["www.shutterstock.com"], 
+  },
 };
 
 export default nextConfig;
